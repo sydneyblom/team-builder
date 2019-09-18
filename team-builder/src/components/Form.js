a428466756990d8372b368db4389109328a276a8
Sydney Blom <sydneyblom@#gmail.com>
2019-09-18 13:55:53 -0500

    Setting up form and fixing mistake in app.js

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,41 +1,19 @@
-import React, { useState } from "react";
+import React from 'react'
 
-const Form = props => {
-    console.log("props", props);
-    const [note, setNote] = useState({ Name: "", Email: "", Role:"" });
-  
-    const handleChanges = e => {
-      console.log(note);
 
-      setNote({ ...note, [e.target.name]: e.target.value });
-    };
-  
-    const submitForm = e => {
-      e.preventDefault();
-      props.addNewNote(note);
-      setNote({ title: "", body: "" });
-    };
-  
+function Form() {
     return (
-      <form onSubmit={submitForm}>
-        <label htmlFor="title">Title</label>
-        <input
-          id="title"
-          type="text"
-          name="title"
-          onChange={handleChanges}
-          value={note.title}
-        />
-        <label htmlFor="note">Note</label>
-        <textarea
-          id="note"
-          name="body"
-          onChange={handleChanges}
-          value={note.body}
-        />
-        <button type="submit">Add Note</button>
-      </form>
-    );
-  };
+        <form>
+            <label for="name">Name</label>
+            <input type="text" name="name" />
 
-export default Form;
+            <label for="email">Email</label>
+            <input type="text" name="email" />
+
+            <label for="role">Role</label>
+            <input type="text" name="role" />
+        </form>
+    )
+}
+
+export default Form; 
